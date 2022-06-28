cdc63fb3560bb8520e8b01ca8fe17c7905cf64e8
Damian Cyntler <damian@cyntler.com>
2022-06-28 23:04:39 +0200

    Handle mongodb

--- index.js
+++ index.js
@@ -1,88 +1,118 @@
 const http = require('http');
 const express = require('express');
 const { engine } = require('express-handlebars');
-const { Server } = require("socket.io");
+const { Server } = require('socket.io');
+const mongoose = require('mongoose');
+const { formatDate, randomInteger } = require('./index-utils');
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
 const port = 8000;
-const cities = ['Warszawa', 'Rzeszów', 'Gdańsk', 'Kraków'];
+const points = ['Suszarnia 1', 'Suszarnia 2', 'Suszarnia 3', 'Suszarnia 4'];
+const minTemperature = -10;
+const maxTemperature = 50;
 const warningTemperature = 40;
+const refreshIntervalSec = 5;
+const viewData = {
+  points,
+  warningTemperature,
+  refreshIntervalSec,
+};
 
-const padTo2Digits = (num) => {
-    return num.toString().padStart(2, '0');
-}
-  
-const formatDate = (date) => {
-    return (
-        [
-            date.getFullYear(),
-            padTo2Digits(date.getMonth() + 1),
-            padTo2Digits(date.getDate()),
-        ].join('-') +
-        ' ' +
-        [
-            padTo2Digits(date.getHours()),
-            padTo2Digits(date.getMinutes()),
-            padTo2Digits(date.getSeconds()),
-        ].join(':')
-    );
-}
+let refreshInterval;
 
-const randomInteger = (min, max) => {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-const calculateTemperatures = () => {
-    return {
-        date: formatDate(new Date()),
-        temperatures: cities.map((city) => ({
-            city,
-            value: randomInteger(10, 50)
-        }))
-    }
-}
-
-const emitData = () => {
-    io.emit('data', calculateTemperatures());
-}
-
-app.set('view engine', 'hbs');
-app.engine('hbs', engine({
-    defaultLayout: 'default',
-    extname: '.hbs'
-}));
-
-app.use(express.static('public'));
-
-app.get('/', (_req, res) => {
-    res.render('index', {
-        cities,
-        warningTemperature
+mongoose
+  .connect('mongodb://localhost:27017/temperatures')
+  .then(() => {
+    const TemperaturesLog = mongoose.model('TemperaturesLog', {
+      data: 'Object',
     });
-});
 
-app.use((_, res) => {
-    res.type('text/plain')
-    res.status(404)
-    res.send('404 - Not Found')
-});
+    const calculateTemperatures = () => {
+      const data = {
+        date: formatDate(new Date()),
+        temperatures: points.map((point) => {
+          const temperatureValue = randomInteger(
+            minTemperature,
+            maxTemperature
+          );
 
-app.use((err, _, res) => {
-    console.error(err.message)
-    res.type('text/plain')
-    res.status(500)
-    res.send('500 - Server Error')
-});
+          return {
+            point,
+            value: temperatureValue,
+            valueColor:
+              temperatureValue >= 30
+                ? 'green'
+                : temperatureValue >= 10
+                ? '#F6BE00'
+                : 'blue',
+          };
+        }),
+      };
 
-io.on('connection', () => {
-    emitData();
-});
+      const temperaturesLog = new TemperaturesLog({
+        data,
+      });
 
-setInterval(() => {
-    emitData();
-}, 5000);
+      temperaturesLog.save();
+      return data;
+    };
 
-server.listen(port, () => console.log(`Example app listening on http://localhost:${port} (CTRL + C to exit)`));
+    app.set('view engine', 'hbs');
+    app.engine(
+      'hbs',
+      engine({
+        defaultLayout: 'default',
+        extname: '.hbs',
+      })
+    );
+
+    app.use(express.static('public'));
+
+    app.get('/', (_req, res) => {
+      res.render('index', viewData);
+    });
+
+    app.get('/archive', (_req, res) => {
+      TemperaturesLog.find().then((archiveData) => {
+        res.render('archive', {
+          ...viewData,
+          archiveData: archiveData
+            .map((data) => data.data)
+            .sort((a, b) => new Date(b.date) - new Date(a.date)),
+        });
+      });
+    });
+
+    app.use((_, res) => {
+      res.type('text/plain');
+      res.status(404);
+      res.send('404 - Not Found');
+    });
+
+    app.use((err, _, res) => {
+      console.error(err.message);
+      res.type('text/plain');
+      res.status(500);
+      res.send('500 - Server Error');
+    });
+
+    io.on('connection', (socket) => {
+      clearTimeout(refreshInterval);
+
+      refreshInterval = setInterval(() => {
+        io.emit('data', calculateTemperatures());
+      }, refreshIntervalSec * 1000);
+
+      socket.on('disconnect', () => {
+        clearTimeout(refreshInterval);
+      });
+    });
+
+    server.listen(port, () =>
+      console.log(`Temperatures app listening on http://localhost:${port}`)
+    );
+  })
+  .catch((err) => console.error('MongoDB failed to connect.', err));
